76eb55ef7a1469f232dba0d839174535704d8d4f
Kelvin <kelvinmoe96@gmail.com>
2025-07-26 20:08:39 +0800

    update the css

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+const balanceEl: HTMLElement | null = document.getElementById("balance");
+const incomeAmountEl: HTMLElement | null =
+  document.getElementById("income-amount");
+const expenseAmountEl: HTMLElement | null =
+  document.getElementById("expense-amount");
+const transactionListEl: HTMLElement | null =
+  document.getElementById("transaction-list");
+const transactionFormEl: HTMLElement | null =
+  document.getElementById("transaction-form");
+const descriptionEl: HTMLElement | null =
+  document.getElementById("description");
+const amountEl = document.getElementById("amount");
